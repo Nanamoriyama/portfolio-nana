31a929091d9ca32e82d8e3800e552f376c9eb568
Nanamoriyama <nanamoriyama@outlook.jp>
2024-08-24 17:18:46 +0200

    fixed1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import React, { useRef, useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import styles from "./Projects.module.css";
+import styles from "./projects.module.css";
 
 const Projects = () => {
   const scrollContainerRef = useRef<HTMLDivElement | null>(null);
